90a8bd1b06b4310a7fe0e2c2080473e8e6f412ba
Raja <rajalingam210901@gmail.com>
2025-04-11 19:18:43 +0530

    completed desigin image scroll bar & javascript function

--- imageCour.js
+++ imageCour.js
@@ -1,47 +1,73 @@
-const imaUrlList=[
-   "./new movie list/spiderman.jpg",
-   " ./new movie list/spiderman.jpg",
-   "./new movie list/Captain America.jpg" ,
-   "./new movie list/batman.jpg"
-];
 
-const aritcalContainer=document.getElementById("item");  
+let scrollContainer=document.querySelector('.image-List');
+let nextButton=document.getElementById('nextButton');
+let backButton=document.getElementById('backButton');
 
-// aritcalContainer.innerHTML=`<img src="${imaUrlList[0]}" />`;
+scrollContainer.addEventListener("wheel",(evt)=>{
+   evt.preventDefault();
+   scrollContainer.scrollLeft += evt.deltaY;
+   scrollContainer.style.scrollBehavior="smooth";
 
-for(let i=0;i<2  && i<imaUrlList.length;i++){
-   const img=document.createElement("img");
-   img.src=imaUrlList[i];
-   aritcalContainer.appendChild(img);
-}
+})
+
+nextButton.addEventListener("click",()=>{
+   scrollContainer.scrollLeft += 850;
+   scrollContainer.style.scrollBehavior="smooth";
+})
+
+backButton.addEventListener("click",()=>{
+   scrollContainer.scrollLeft -= 850;
+   scrollContainer.style.scrollBehavior="smooth";
+
+})
 
 
 
 
-let imgIndex=0;
-// prev image function
-function prevImage(){
- if(imgIndex > 0 && imgIndex<imaUrlList.length){
-   imgIndex--;
- }
- else{
-   imgIndex=imaUrlList.length-1;
- }
- aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
-}
+// const imaUrlList=[
+//    "./new movie list/spiderman.jpg",
+//    " ./new movie list/spiderman.jpg",
+//    "./new movie list/Captain America.jpg" ,
+//    "./new movie list/batman.jpg"
+// ];
 
-// next image fucntion
-function nextImage(){
-   if(imgIndex > 0 && imgIndex<imaUrlList.length-1){
-      imgIndex++;
-   }else{
-      imgIndex=0;
-   }
- aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
+// const aritcalContainer=document.getElementById("item");  
+
+// // aritcalContainer.innerHTML=`<img src="${imaUrlList[0]}" />`;
+
+// for(let i=0;i<2  && i<imaUrlList.length;i++){
+//    const img=document.createElement("img");
+//    img.src=imaUrlList[i];
+//    aritcalContainer.appendChild(img);
+// }
+
+
+
+
+// let imgIndex=0;
+// // prev image function
+// function prevImage(){
+//  if(imgIndex > 0 && imgIndex<imaUrlList.length){
+//    imgIndex--;
+//  }
+//  else{
+//    imgIndex=imaUrlList.length-1;
+//  }
+//  aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
+// }
+
+// // next image fucntion
+// function nextImage(){
+//    if(imgIndex > 0 && imgIndex<imaUrlList.length-1){
+//       imgIndex++;
+//    }else{
+//       imgIndex=0;
+//    }
+//  aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
  
 
-}
+// }
 
-var a=["aprile","orange","banana","graps"];
+// var a=["aprile","orange","banana","graps"];
 
-console.log(a.slice(1,3)); 
+// console.log(a.slice(1,3)); 
