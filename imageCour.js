64f6ecc6edf220a859e441abc936ffcb46883ebc
Raja <rajalingam210901@gmail.com>
2025-04-11 19:49:42 +0530

    work with nav function

--- imageCour.js
+++ imageCour.js
@@ -21,53 +21,15 @@ backButton.addEventListener("click",()=>{
 
 })
 
+// nav bar 
+
+const activePage=window.location.pathname;
 
 
+const navBar=document.querySelectorAll(".nav-bar ul a").forEach(link=>{
+   if(link.href.includes(`${activePage}`)){
+      console.log(`${activePage}`);
+   };
+});
+console.log(navBar);
 
-// const imaUrlList=[
-//    "./new movie list/spiderman.jpg",
-//    " ./new movie list/spiderman.jpg",
-//    "./new movie list/Captain America.jpg" ,
-//    "./new movie list/batman.jpg"
-// ];
-
-// const aritcalContainer=document.getElementById("item");  
-
-// // aritcalContainer.innerHTML=`<img src="${imaUrlList[0]}" />`;
-
-// for(let i=0;i<2  && i<imaUrlList.length;i++){
-//    const img=document.createElement("img");
-//    img.src=imaUrlList[i];
-//    aritcalContainer.appendChild(img);
-// }
-
-
-
-
-// let imgIndex=0;
-// // prev image function
-// function prevImage(){
-//  if(imgIndex > 0 && imgIndex<imaUrlList.length){
-//    imgIndex--;
-//  }
-//  else{
-//    imgIndex=imaUrlList.length-1;
-//  }
-//  aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
-// }
-
-// // next image fucntion
-// function nextImage(){
-//    if(imgIndex > 0 && imgIndex<imaUrlList.length-1){
-//       imgIndex++;
-//    }else{
-//       imgIndex=0;
-//    }
-//  aritcalContainer.innerHTML=`<img src="${imaUrlList[imgIndex]}" />`;
- 
-
-// }
-
-// var a=["aprile","orange","banana","graps"];
-
-// console.log(a.slice(1,3)); 
