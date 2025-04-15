e19b4a15bb9eafd2f56f7b002e1333d8601706c7
Raja <rajalingam210901@gmail.com>
2025-04-15 21:25:25 +0530

    added nav bar js file & desigin

--- imageCour.js
+++ imageCour.js
@@ -42,11 +42,5 @@ backButton.addEventListener("click",()=>{
 // };
 
 
-const activePage=window.location.pathname;
-console.log(activePage);
-const navLinks=document.querySelectorAll("nav a").forEach(link=>{
-if(link.href.includes(`${activePage}`)){
-   link.classList.add('active');
-}
-})
+
 
---
+++ navbar.js
@@ -0,0 +1,7 @@
+const activePage=window.location.pathname;
+console.log(activePage);
+const navLinks=document.querySelectorAll("nav a").forEach(link=>{
+if(link.href.includes(`${activePage}`)){
+   link.classList.add('active');
+}
+});
